13d7af3530ee1e4b13095bbefd57f7e04e848eed
Taylor Mason <taylorwmason@gmail.com>
2017-03-20 22:37:19 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+	<div id="app">
+		<Signin></Signin>
+		<md-menu>
+			<md-button md-menu-trigger>Bottom Right</md-button>
+			<md-menu-content>
+				<md-menu-item>My Item 1</md-menu-item>
+				<md-menu-item>My Item 2</md-menu-item>
+				<md-menu-item>My Item 3</md-menu-item>
+			</md-menu-content>
+		</md-menu>
+	</div>
+</template>
+
+<script>
+import Signin from './components/Signin'
+import VueMaterial from 'vue-material'
+
+export default {
+  name: 'app',
+  components: {
+    Signin, VueMaterial
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Signin.vue
@@ -0,0 +1,48 @@
+<template>
+  	<div class="signin">
+	  	<form novalidate @submit.stop.prevent="submit">
+			<md-input-container>
+    			<label>Username</label>
+    			<md-textarea></md-textarea>
+			</md-input-container>
+			<md-input-container md-has-password>
+    			<label>Password</label>
+    			<md-input type="password"></md-input>
+			</md-input-container>
+			<md-button class="md-raised md-primary">Submit</md-button>
+
+		</form>
+	</div>
+</template>
+
+<script>
+export default {
+  name: 'signin',
+  data () {
+    return {
+      msg: 'Please Login'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
